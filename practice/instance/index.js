3fad1819474f780f1f1948d6dc1e26e4c813e12b
daniel,yip <yeshuijin@outlook.com>
2020-02-04 17:09:12 +0800

    updata

--- practice/instance/index.js
+++ practice/instance/index.js
@@ -1,6 +1,47 @@
 import Vue from 'vue'
 
-new Vue({
+const app = new Vue({
   el: '#root',
-  template: '<div>this is a div</div>'
+  data: {
+    text: 0
+  },
+  watch: {
+    text (oldText, newText) {
+      console.log(`${oldText} : ${newText}`)
+    }
+  },
+  template: '<div>{{text}}</div>'
 })
+
+setInterval(() => {
+  app.text += 1
+}, 1000)
+
+console.log(app.$el)
+console.log(app.$data)
+console.log(app.$props)
+console.log(app.$options)
+console.log(app.$root)
+console.log(app.$children)
+console.log(app.$slots)
+console.log(app.$scopedSlots)
+console.log(app.$refs)
+console.log(app.$isServer)
+
+const unwatch = app.$watch('text', (oldText, newText) => {
+  console.log(`${oldText} : ${newText}`)
+})
+
+unwatch()
+
+app.$options.render = (h) => {
+  return h('div', {}, 'new Render function')
+}
+
+app.$on('test', (a, b) => {
+  console.log(`test emited, ${a}, ${b}`)
+})
+
+app.$emit('test', 1, 2)
+
+app.$forceUpdate()
